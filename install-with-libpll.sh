8a9b384b916970c81cd437e73e54d4fd83fdfe0d
kozlov <alexey.kozlov@h-its.org>
2017-04-19 14:39:47 +0200

    allow to disable SSE/AVX vectorization

--- install-with-libpll.sh
+++ install-with-libpll.sh
@@ -10,15 +10,27 @@ else
   PLLMOD_PREFIX_ARG="$PREFIX_ARG CPPFLAGS="-I$PREFIX/include/libpll" LDFLAGS="-L$PREFIX/lib""
 fi
 
+if [ "$#" -ge 2 ]; then
+  PLL_ARGS=$2
+else
+  PLL_ARGS=
+fi
+
+if [ "$#" -ge 3 ]; then
+  PLLMOD_ARGS=$3
+else
+  PLLMOD_ARGS=
+fi
+
 # configure & install libpll
 cd libs/libpll
 ./autogen.sh
-./configure $PREFIX_ARG
+./configure $PREFIX_ARG $PLL_ARGS
 make install
 
 # configure & install modules
 cd ../..
 autoreconf --force --install
-./configure $PLLMOD_PREFIX_ARG
+./configure $PLLMOD_PREFIX_ARG $PLLMOD_ARGS
 make install
 
